86ba0260c4d8736d2c5b9c0d6f1f09d8813c39b1
Keerthik Shenoy <Keerthik9879@gmail.com>
2023-08-02 09:34:58 +0530

    Update ami-backup-create-delete.py

--- ami-backup-create-delete.py
+++ ami-backup-create-delete.py
@@ -5,8 +5,11 @@ def create_ami(instance_id, image_name, region, profile_name):
     session = boto3.Session(region_name=region, profile_name=profile_name)
     ec2_client = session.client('ec2')
 
-    # Add InstanceId tag
-    tags = [{'Key': 'InstanceId', 'Value': instance_id}]
+    # Add InstanceId and autoexpiry tags
+    tags = [
+        {'Key': 'InstanceId', 'Value': instance_id},
+        {'Key': 'autoexpiry', 'Value': 'true'}
+    ]
 
     try:
         response = ec2_client.create_image(
@@ -35,6 +38,7 @@ def delete_old_amis(instance_ids, region, profile_name, retention_days):
     response = ec2_client.describe_images(
         Filters=[
             {'Name': 'tag:InstanceId', 'Values': instance_ids},
+            {'Name': 'tag:autoexpiry', 'Values': ['true']},
             {'Name': 'state', 'Values': ['available']}
         ]
     )
@@ -42,20 +46,27 @@ def delete_old_amis(instance_ids, region, profile_name, retention_days):
 
     for image in images:
         image_id = image['ImageId']
-        snapshot_id = ''
-        for mapping in image['BlockDeviceMappings']:
-            if 'Ebs' in mapping and 'SnapshotId' in mapping['Ebs']:
-                snapshot_id = mapping['Ebs']['SnapshotId']
-                break
+        creation_date = image['CreationDate']
+        image_date = datetime.strptime(creation_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        age = datetime.now() - image_date
 
-        if snapshot_id:
-            try:
-                ec2_client.deregister_image(ImageId=image_id)
-                print(f"Deleted AMI {image_id} for instance IDs: {', '.join(instance_ids)} in region {region}")
-                ec2_client.delete_snapshot(SnapshotId=snapshot_id)
-                print(f"Deleted Snapshot {snapshot_id} for AMI {image_id} in region {region}")
-            except Exception as e:
-                print(f"Failed to delete AMI {image_id} for instance IDs: {', '.join(instance_ids)} in region {region}: {str(e)}")
+        if age.days >= retention_days:
+            snapshot_id = ''
+            for mapping in image['BlockDeviceMappings']:
+                if 'Ebs' in mapping and 'SnapshotId' in mapping['Ebs']:
+                    snapshot_id = mapping['Ebs']['SnapshotId']
+                    break
+
+            if snapshot_id:
+                try:
+                    ec2_client.deregister_image(ImageId=image_id)
+                    print(f"Deleted AMI {image_id} for instance IDs: {', '.join(instance_ids)} in region {region}")
+                    ec2_client.delete_snapshot(SnapshotId=snapshot_id)
+                    print(f"Deleted Snapshot {snapshot_id} for AMI {image_id} in region {region}")
+                except Exception as e:
+                    print(f"Failed to delete AMI {image_id} for instance IDs: {', '.join(instance_ids)} in region {region}: {str(e)}")
+        else:
+            print(f"AMI {image_id} is not eligible for deletion. It has not reached the retention period yet.")
 
 def backup_and_delete_amis(instance_dict, profile_name, retention_days):
     for region, instances in instance_dict.items():
@@ -69,9 +80,9 @@ def backup_and_delete_amis(instance_dict, profile_name, retention_days):
             image_id = create_ami(instance_id, today_image_name, region, profile_name)
             if image_id:
                 print(f"Created AMI {today_image_name} (ID: {image_id}) for instance {instance_id}")
+                delete_old_amis([instance_id], region, profile_name, retention_days)
             else:
                 print(f"Failed to create AMI for instance {instance_id}")
-            delete_old_amis([instance_id], region, profile_name, retention_days)
 
 # Specify your instance IDs, regions, profile name, and retention period (in days) here
 instance_dict = {
